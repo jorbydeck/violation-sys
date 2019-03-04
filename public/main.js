b5addb21cd356226f6b5c823ea1b9531eac3b68a
jorbydeck <jordanwifwafu@yahoo.com>
2019-03-04 16:20:29 +0800

    odifying start script adding BROWSER=none and wait-on using concurrently

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -6,10 +9,12 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 900, height: 680 })
+
 
   // and load the index.html of the app.
-  win.loadURL('http://localhost:3000/')
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  //win.loadURL('http://localhost:3000/')
 
   // Open the DevTools.
   //win.webContents.openDevTools()
